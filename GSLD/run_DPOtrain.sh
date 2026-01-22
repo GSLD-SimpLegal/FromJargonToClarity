5d1417efad9718ba64158e9b603af3a1d76bfddf
GSLD-SimpLegal <hiranmai.sri@research.iiit.ac.in>
2026-01-22 12:27:14 +0530

    Delete GSLD/run_DPOtrain.sh

--- GSLD/run_DPOtrain.sh
+++
@@ -1,5 +0,0 @@
-python DPO_trainer.py \
-  --model "model name" \
-  --input_file "path to data (.json)" \
-  --grade "target grade (skilled|intermediate|basic)" \
-  --hf_token hf_xxxxxxxxxxxxxxxxxxxxx
