35d2b1fdd4ecf60a22143b393eb9ed5ee0b796ca
GSLD-SimpLegal <hiranmai.sri@research.iiit.ac.in>
2026-01-22 12:27:32 +0530

    Delete GSLD/run_TextSimplification.sh

--- GSLD/run_TextSimplification.sh
+++
@@ -1,9 +0,0 @@
-python ${PYTHON_FILE} \
-  --hf_token "${HF_TOKEN}" \
-  --model_path "${MODEL_PATH}" \
-  --context "${CONTEXT}" \
-  --memory_limit "${MEMORY_LIMIT}" \
-  --method "${METHOD}" \
-  --granularity "${GRANULARITY}" \
-  --model_name "${MODEL_NAME}" \
-  --memory_file_name "${MEMORY_FILE}"
